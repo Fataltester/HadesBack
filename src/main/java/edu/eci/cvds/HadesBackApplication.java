b498bd6ed5b88d974699799328c843515c78e13c
Fataltester <juanm46891@gmail.com>
2025-04-23 19:54:35 -0500

    estructura del proyecto en spring

---
+++ src/main/java/edu/eci/cvds/HadesBackApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadesBackApplication.class, args);
+	}
+
+}
