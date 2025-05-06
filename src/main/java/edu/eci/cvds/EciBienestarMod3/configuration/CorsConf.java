ddcbfe9d17f6708f73889fa9f0122c63d61cb6c8
Alejandro PR <samuel.prieto-r@mail.escuelaing.edu.co>
2025-05-05 23:27:13 -0500

    Agregando configuracion de cors, Para aceptar peticiones del (localhost:3000) para front ya que es con react local y sobre los metodos (get, post, put delete), tambien, puse sobre aceptar las peticiones

---
+++ src/main/java/edu/eci/cvds/EciBienestarMod3/configuration/CorsConf.java
@@ -0,0 +1,8 @@
+package edu.eci.cvds.EciBienestarMod3.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConf {
+
+}
---
+++ src/main/java/edu/eci/cvds/EciBienestarMod3/configuration/SecurityConf.java
@@ -0,0 +1,36 @@
+package edu.eci.cvds.EciBienestarMod3.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConf {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000"
+        ));
+
+        corsConfiguration.setAllowedHeaders(Arrays.asList(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE"
+        ));
+
+        corsConfiguration.setAllowedHeaders(Arrays.asList(
+                "*"
+        ));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration);
+
+        return source;
+    }
+}
